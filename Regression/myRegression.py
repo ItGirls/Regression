62b5dd33802046a4d46e2b4d5258cb275d7ea89d
ItGirls <582353466@qq.com>
2018-08-11 21:39:05 +0800

    Update myRegression.py

--- Regression/myRegression.py
+++ Regression/myRegression.py
@@ -56,30 +56,30 @@ class MyRegression():
 
         # print self.trainData
 if __name__ == '__main__':
-    DataFile = '/Users/tingtingzhu/学习书籍/prml/practice/data.txt'
+    DataFile = 'data.txt'
 
     with open(DataFile) as f:
         #split data
         myDemo = MyRegression(f)
         myDemo.splitData(0.7)
 
-        # #1.bayesis regression
-        # #esitimate
-        # mybayesis = BayesianRegression(myDemo.wholeData,myDemo.N)
-        #
-        # mybayesis.getDesignMatrix(myDemo.trainsNum, myDemo.tempTrainIndicies,
-        #                           myDemo.tempTestIndicies,10,basisfun.powerBasis)
-        # mybayesis.initialize()
-        # mybayesis.esitimateParameters()
-        #
-        # #predict
-        # mybayesis.predict()
-        # # mybayesis.error()
-        # hatY = np.array(mybayesis.predictT)[:, 0]
-        # realY = mybayesis.testT
-        #
-        # # measure
-        # MeasureChoice.rmse(realY, hatY, myDemo.testsNum)
+        #1.bayesis regression
+        #esitimate
+        mybayesis = BayesianRegression(myDemo.wholeData,myDemo.N)
+        
+        mybayesis.getDesignMatrix(myDemo.trainsNum, myDemo.tempTrainIndicies,
+                                  myDemo.tempTestIndicies,10,basisfun.powerBasis)
+        mybayesis.initialize()
+        mybayesis.esitimateParameters()
+        
+        #predict
+        mybayesis.predict()
+        # mybayesis.error()
+        hatY = np.array(mybayesis.predictT)[:, 0]
+        realY = mybayesis.testT
+        
+        # measure
+        MeasureChoice.rmse(realY, hatY, myDemo.testsNum)
 
         # #2.normal equeation + regularized least squares
         # mymodel = MLRegression(myDemo.wholeData, myDemo.N,12)
@@ -100,20 +100,20 @@ if __name__ == '__main__':
         # MeasureChoice.rmse(realY, hatY, myDemo.testsNum)
 
 
-        #3. least squares with stochastic gradient decent
-        mymodel = MLRegression1(myDemo.wholeData, myDemo.N,10)
+#         #3. least squares with stochastic gradient decent
+#         mymodel = MLRegression1(myDemo.wholeData, myDemo.N,10)
 
-        mymodel.getDesignMatrix(myDemo.trainsNum, myDemo.tempTrainIndicies,
-                                  myDemo.tempTestIndicies, basisfun.powerBasis)
-        mymodel.initialize()
-        mymodel.esitimateParameters()
+#         mymodel.getDesignMatrix(myDemo.trainsNum, myDemo.tempTrainIndicies,
+#                                   myDemo.tempTestIndicies, basisfun.powerBasis)
+#         mymodel.initialize()
+#         mymodel.esitimateParameters()
 
-        # predict
-        mymodel.predict()
-        # mybayesis.error()
-        hatY = np.array(mymodel.predictT)[:, 0]
-        realY = mymodel.testT
-        # realY = mymodel.t
+#         # predict
+#         mymodel.predict()
+#         # mybayesis.error()
+#         hatY = np.array(mymodel.predictT)[:, 0]
+#         realY = mymodel.testT
+#         # realY = mymodel.t
 
-        # measure
-        MeasureChoice.rmse(realY, hatY, myDemo.testsNum)
+#         # measure
+#         MeasureChoice.rmse(realY, hatY, myDemo.testsNum)
